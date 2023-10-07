40a722f029ad922c1b4454f09d64683166b56fe0
Shubham Singh <shubhk530@gmail.com>
2023-10-07 20:09:57 +0530

    dataTypes summary

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -4,9 +4,9 @@
 
 //code readablity should bw high.
 
-let name = 'shubham'
-let age = 25
-let isMale = true 
+// let name = 'shubham'
+// let age = 25
+// let isMale = true 
 
 
 //number => 2^53, 2 to power 53
@@ -20,7 +20,52 @@ let isMale = true
 
 //object
 
-console.log(typeof null)//object
-console.log(typeof undefined)//undefined
+// console.log(typeof null)//object
+// console.log(typeof undefined)//undefined
+
+//************************************ */
+
+//Primitive DataTypes => 7 Types
+// String, Number, Boolean, Null, Undefined, Symbol, Bigint
+
+//const num = 1
+//const bool = true
+// const bar = null 
+//const boo = undefined
+// const name = 'Shubham'
+// const id = Symbol('123');
+// const studentId = Symbol('123');
+// console.log(id)
+// console.log(studentId)
+// console.log(studentId===id)
+
+// const bigNumber = 785456565666666666645n
+// console.log(bigNumber, typeof bigNumber)
+
+
+//Reference Types or Non-Primitive
+// Arrays, Objects, Functions
+
+const heroes = ['Shaktimaan', 'NaagRaaj', 'Doga']
+
+let obj = {
+    name:'Shubham',
+    rollNo:15533,
+    department:'CSE',
+    data:heroes
+}
+
+const func1= function (params) {
+    console.log("this is a function")
+}
+
+// console.log(typeof id)
+console.log(typeof heroes)
+console.log(typeof obj)
+console.log(typeof func1)
+// JavaScript is A Dynamically Typed Language
+
+
+
 
 
