fccc2980f30cd4e554a75cb276051a742c5043c2
Shubham Singh <shubhk530@gmail.com>
2023-10-07 18:48:07 +0530

    datatypes revision

---
+++ basics/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) we are using nodejs, not browser
+
+//code readablity should bw high.
+
+let name = 'shubham'
+let age = 25
+let isMale = true 
+
+
+//number => 2^53, 2 to power 53
+//bigint => no limit
+// string => ""
+// boolean => true/false
+//null => standalone value
+//undefined => no value assigned
+//symbol => unique
+
+
+//object
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+
+
---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144458;
+let accountName = 'Shubham@gmail.com'
+var accountPassword = '1457896'
+accountCity = 'Jaipur'
+console.table([accountId,accountName,accountPassword,accountCity])
+
+// accountId = 2; Assignment to constanr variable is not allowed.
+accountName = "shubham Singh"
+accountPassword = '4578966'
+accountCity = 'Delhi'
+console.table([accountId,accountName,accountPassword,accountCity])
+
+// Prefer not to use var 
+// because of issue with block scope or functional scope 
