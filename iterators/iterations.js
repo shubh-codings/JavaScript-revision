bef8926f5cb309487548c079bd022744d9f74022
Shubham Singh <shubhk530@gmail.com>
2023-10-08 15:01:15 +0530

    Iteration in js

---
+++ iterators/iterations.js
@@ -0,0 +1,95 @@
+// For Loops
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i) 
+// }
+
+// for (let i = 0; i <= 5; i++) {
+//     console.log(`Outer Loop value ${i}`);
+//     for (let j = 0; j <= 5; j++) {
+//         console.log(`inner loop value ${j} and outer loop value${i}`);
+        
+//     }
+    
+// }
+
+let myArr = ['Flash', 'Superman', 'Thor','Hulk','Bat-Man', 'Iron-Man'];
+
+// for (let i = 0; i < myArr.length; i++) {
+//     const element = myArr[i];
+//     console.log(element)
+    
+// }
+
+//Break and Continue
+
+// for (let i = 1; i < 20; i++) {
+//     const element = i;
+//     if(i==5){
+//         console.log("You have reached the 5 element")
+//         break;
+//     }
+//     console.log(`value of i is ${element}`)
+    
+// }
+// output
+// value of i is 1
+// value of i is 2
+// value of i is 3
+// value of i is 4
+// You have reached the 5 element
+
+
+
+// for (let i = 1; i <= 20; i++) {
+//     const element = i;
+//     if(i==5){
+//         console.log("You have reached the 5 element")
+//         continue;
+//     }
+//     console.log(`value of i is ${element}`)
+    
+// }
+//output
+
+// value of i is 1
+// value of i is 2
+// value of i is 3
+// value of i is 4
+// You have reached the 5 element
+// value of i is 6
+// value of i is 7
+// value of i is 8
+// value of i is 9
+// value of i is 10
+// value of i is 11
+// value of i is 12
+// value of i is 13
+// value of i is 14
+// value of i is 15
+// value of i is 16
+// value of i is 17
+// value of i is 18
+// value of i is 19
+// value of i is 20
+
+
+//While loop
+
+let index = 11;
+// while(index <=10){
+//     console.log(index*10)
+//     index = index+1
+// }
+
+// while (index<myArr.length){
+//     console.log(myArr[index]);
+//     index++;
+// }
+
+//do while loop run atleast one time
+// do {
+//     console.log(`score is ${index}`)
+//     index++;
+// }while(index<10)
+
